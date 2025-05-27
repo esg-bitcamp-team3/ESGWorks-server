34e15d5927a9507f028113ff312821a1dbf0cab3
Sangjin Jeong <sangjinjeong@Sangjinui-MacBookPro.local>
2025-05-27 23:49:14 +0900

    no error

--- src/main/java/com/esgworks/service/UserService.java
+++ src/main/java/com/esgworks/service/UserService.java
@@ -16,6 +16,11 @@ public class UserService {
     private PasswordEncoder passwordEncoder;
 
     public boolean signup(UserSignupRequest req) {
+
+
+        if (userRepository.existsById(req.getId())) {
+            return false; // 중복 이메일
+        }
         if (userRepository.existsByEmail(req.getEmail())) {
             return false; // 중복 이메일
         }
@@ -24,7 +29,9 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(req.getPassword())); // 비밀번호 암호화!
         user.setName(req.getName());
         user.setPhoneNumber(req.getPhoneNumber());
+        user.setCorpId(req.getCorpId());
         userRepository.save(user);
         return true;
     }
-}
+    }
+
