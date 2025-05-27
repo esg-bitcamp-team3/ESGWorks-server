4286f4ec9efcaff4a3e67adc544c2741ec96ec19
Sangjin Jeong <sangjinjeong@Sangjinui-MacBookPro.local>
2025-05-28 00:03:59 +0900

    build structure

--- src/main/java/com/esgworks/domain/UserDocument.java
+++ src/main/java/com/esgworks/domain/UserDocument.java
@@ -1,14 +1,16 @@
 package com.esgworks.domain;
 
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
 @Document(collection = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserDocument {
     @Id
     private String id;
